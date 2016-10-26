19d1e0665c379c0562222c314ad279ab38044e8f
Harm Weites <harm@weites.com>
2016-10-26 21:13:04 +0200

    Fix up metric names

--- exporter/exporter.py
+++ exporter/exporter.py
@@ -22,26 +22,26 @@ def process(raw_data, zone):
         # This is a Cloudflare limitation.
         serie = popdata['timeseries'][-7]
         print ("%s | %s" % (serie['since'], serie['until']))
-        families['requests'].add_metric([zone, 'cached', popdata['colo_id']], serie['requests']['cached'])
-        families['requests'].add_metric([zone, 'uncached', popdata['colo_id']], serie['requests']['uncached'])
+        families['received_requests'].add_metric([zone, 'cached', popdata['colo_id']], serie['requests']['cached'])
+        families['received_requests'].add_metric([zone, 'uncached', popdata['colo_id']], serie['requests']['uncached'])
 
-        families['bandwidth'].add_metric([zone, 'cached', popdata['colo_id']], serie['bandwidth']['cached'])
-        families['bandwidth'].add_metric([zone, 'uncached', popdata['colo_id']], serie['bandwidth']['cached'])
+        families['bandwidth_bytes'].add_metric([zone, 'cached', popdata['colo_id']], serie['bandwidth']['cached'])
+        families['bandwidth_bytes'].add_metric([zone, 'uncached', popdata['colo_id']], serie['bandwidth']['cached'])
 
         for http_status, value in serie['requests']['http_status'].items():
-            families['http_response'].add_metric([zone, popdata['colo_id'], http_status], value)
+            families['http_responses_send'].add_metric([zone, popdata['colo_id'], http_status], value)
 
-        families['threats'].add_metric([zone, popdata['colo_id']], serie['threats']['all'])
+        families['threats_seen'].add_metric([zone, popdata['colo_id']], serie['threats']['all'])
 
         for threat, value in serie['threats']['type'].items():
             families['threat_types'].add_metric([zone, popdata['colo_id'], threat], value)
 
     families = {
-        'requests':      GaugeMetricFamily('cloudflare_pop_requests', 'Requests received at this PoP location.', labels=['zone', 'type', 'colo_id']),
-        'bandwidth':     GaugeMetricFamily('cloudflare_pop_bandwidth', 'Bandwidth send from this PoP location.', labels=['zone', 'type', 'colo_id']),
-        'http_response': GaugeMetricFamily('cloudflare_pop_http_response', 'Breakdown per HTTP response code.', labels=['zone', 'type', 'colo_id']),
-        'threats':       GaugeMetricFamily('cloudflare_pop_threats', 'Threats identified.', labels=['zone', 'colo_id', 'threats']),
-        'threat_types':  GaugeMetricFamily('cloudflare_pop_threat_types', 'Threat breakdown per threat type.', labels=['zone', 'colo_id', 'threat_type'])
+        'received_requests':   GaugeMetricFamily('cloudflare_pop_received_requests', 'Requests received at this PoP location.', labels=['zone', 'type', 'colo_id']),
+        'bandwidth_bytes':     GaugeMetricFamily('cloudflare_pop_bandwidth_bytes', 'Bandwidth send from this PoP location.', labels=['zone', 'type', 'colo_id']),
+        'http_responses_send': GaugeMetricFamily('cloudflare_pop_http_responses_send', 'Breakdown per HTTP response code.', labels=['zone', 'type', 'colo_id']),
+        'threats_seen':        GaugeMetricFamily('cloudflare_pop_threats_seen', 'Threats identified.', labels=['zone', 'colo_id', 'threats']),
+        'threat_types':        GaugeMetricFamily('cloudflare_pop_threat_types', 'Threat breakdown per threat type.', labels=['zone', 'colo_id', 'threat_type'])
     }
 
     for popdata in raw_data:
